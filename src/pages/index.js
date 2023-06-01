a5aaa4f9371c0b86a8f4df9557dfed74833778da
Paul Kim <kimbaudi@gmail.com>
2023-06-01 15:17:24 -0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { css } from "@emotion/react";
-import MyButton from "@kimbaudi/testreactjslib";
+import MyButton from "@kimbaudi/testreacttslib";
 
 const IndexPage = () => {
   return (
