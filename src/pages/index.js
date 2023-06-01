79196f6c93bc68a63ce6384cfe1dcfb2f4a855e7
Paul Kim <kimbaudi@gmail.com>
2023-06-01 15:14:13 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { css } from "@emotion/react";
+import MyButton from "@kimbaudi/testreactjslib";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <button
+        css={css`
+          background-color: red;
+          color: white;
+          padding: 7px 14px;
+          &:hover {
+            background-color: purple;
+          }
+        `}
+      >
+        Button
+      </button>
+      <MyButton />
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
